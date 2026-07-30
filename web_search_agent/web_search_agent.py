d11d5b84e65493c171753c7c8c71ffb8e0ee424c
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2026-07-30 17:23:16 -0400

    Packages web search agent as a resuable class `WebSearchAgent`

--- web_search_agent/web_search_agent.py
+++ web_search_agent/web_search_agent.py
@@ -1,23 +1,32 @@
 from agents import WebSearchTool, Agent, Runner, Model
 from asyncio import run
 
-search_tool = WebSearchTool(
-    search_context_size='medium', # takes value 'low', 'medium', 'high'
-    external_web_access = True
-)
+class WebSearchAgent():
+    def __init__(self):
 
-agent = Agent(
-    name='web_search', 
-    instructions="""You are an assistant that is given context about a user's inquiry,
-and formulates any web searches that may aid in responding to the inquiry.""",
-    tools=[search_tool]
-)
+        self._search_tool = WebSearchTool(
+            search_context_size='medium', # takes value 'low', 'medium', 'high'
+            external_web_access = True
+        )
+        self._agent = Agent(
+            name='web_search', 
+            instructions="""You are an assistant that is given context about a user's inquiry,
+        and formulates any web searches that may aid in responding to the inquiry.""",
+            tools=[self._search_tool]
+        )
 
+    async def answer(self, question: str) -> str:
+        runner = Runner()
+        result = await runner.run(self._agent, input=question)
+        return result.final_output
+
+
+# For running interactively:
 async def main():
-    initial_context = input("Initial context for the web search agent: ")
-    runner = Runner()
-    result = await runner.run(agent, input=initial_context)
-    print(result.final_output)
+    interactive_question = input("Initial context for the web search agent: ")
+    web_search_agent = WebSearchAgent()
+    interactive_answer = await web_search_agent.answer(interactive_question)
+    print(interactive_answer)
 
 if __name__ == '__main__':
     run(main())
